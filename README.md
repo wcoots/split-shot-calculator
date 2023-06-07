f8e39ee901627ad9428aba22088bf8f49397087f
WillCooter <will@cooters.co.uk>
2023-06-07 12:19:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Split-shot calculator app
+
+A tool for calculating what sizes of split shot to use for a float.
+
+
+### Commands
+-   `npm start` - run the app in development mode.
+-   `npm run build`- build the app for production.
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './index.css';
+
+function App() {
+    return (
+        <div className="container">
+            <div className="header">
+                <b>Split-shot calculator</b>
+                <a
+                    href="https://github.com/wcoots/split-shot-calculator"
+                    target="_blank"
+                    rel="noreferrer">
+                    <img src="github.svg" alt="GitHub" />
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export default App;
